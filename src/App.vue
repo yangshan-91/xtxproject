25c85f0155efe0da671ef4f1144dca19223d9ddd
yangshan-91 <2533896613@qq.com>
2024-08-16 08:51:42 +0800

    scss文件的自动导入

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,16 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
    <RouterView />
+   <div class="test">
+     test scss
+   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.test {
+  color:$priceColor ;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;

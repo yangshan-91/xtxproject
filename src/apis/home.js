94d6fa153357eb29ba017af20a5ec253909b69d0
yangshan-91 <2533896613@qq.com>
2024-08-25 10:53:44 +0800

    Home-banner轮播图

---
+++ src/apis/home.js
@@ -0,0 +1,12 @@
+/**
+ * @description: 获取banner图
+ * @param {*}
+ * @return {*}
+ */
+
+import  httpInstance  from '@/utils/http'
+export function getBannerAPI (){
+  return httpInstance ({
+    url:'home/banner'
+  })
+}

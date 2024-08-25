a09039806e47b5afd8b6fc289b0ae6772b7e21ed
yangshan-91 <2533896613@qq.com>
2024-08-25 21:52:22 +0800

    Home-图片懒加载指令实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -10,3 +10,25 @@ export function getBannerAPI (){
     url:'home/banner'
   })
 }
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => {
+  return httpInstance({
+    url:'/home/new'
+  })
+}
+
+/**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
+export const getHotAPI = () => {
+  return httpInstance({
+    url: '/home/hot'
+  })
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 //引入初始化样式文件
 import '@/styles/common.scss'
-
+import { useIntersectionObserver } from '@vueuse/core'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -13,3 +13,23 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+//定义全局指令
+app.directive('img-lazy', {
+    mounted(el, binding) {
+        //el: 指令所在元素
+        //binding: binding.value 指令等于号后面绑定表达式的值 图片url
+        console.log(el, binding.value)
+        useIntersectionObserver(
+            el,
+            ([{ isIntersecting }], ) => {
+              console.log(isIntersecting)
+              if (isIntersecting) {
+                //进入可视区域
+                  el.src = binding.value
+                  
+              }
+            },
+          )
+    }
+})

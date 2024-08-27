7c4adaa908539cf18cff5166aaa80e1ad7d8f1b5
yangshan-91 <2533896613@qq.com>
2024-08-27 16:05:36 +0800

    一级分类-按照业务逻辑拆分函数

---
+++ src/views/Category/composables/useBanner.js
@@ -0,0 +1,19 @@
+import { ref, onMounted } from 'vue'
+import { getBannerAPI } from '@/apis/home'
+
+//封装banner轮播图相关业务代码
+export function useBanner() {
+    const bannerList = ref([])
+    const getBanner = async () => {
+        const res = await getBannerAPI({
+            distributionSite: '2'
+        })
+        console.log(res)
+        bannerList.value = res.result
+    }
+
+    onMounted(() => getBanner())
+    return {
+        bannerList
+    }
+}
